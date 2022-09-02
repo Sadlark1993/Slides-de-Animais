766ff0bfd5e183aabd284789b4fd43757547301c
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-09-02 17:11:20 -0400

    Adicionados eventos ao slide-wrapper
    Eventos adicionados: mousedown, mousemove, mouseup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+import Slide from './slide.js';
+
+
+/* args: chamada da lista (ul) de slides, chamada do bloco wrapper da lista de slides. */
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
