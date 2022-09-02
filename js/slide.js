c303f92688ca13648c39390e33abe88d518e788d
Sadlark1993 <claudio.zi.junior@gmail.com>
2022-09-02 18:07:51 -0400

    Adicionado o Cálculo de Distância quando o Evento Ocorre

--- js/slide.js
+++ js/slide.js
@@ -2,21 +2,39 @@ export default class Slide{
     constructor(slide, wrapper){
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+
+        this.dist = {
+            finalPosition: 0,
+            startX: 0,
+            movement: 0,
+        };
+    }
+
+    moveSlide(distX){
+        this.dist.movePosition = distX;
+        this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+    }
+
+    updatePosition(clientX){
+        this.dist.movement = (clientX -this.dist.startX)*1.6;
+        return this.dist.finalPosition + this.dist.movement;
     }
 
     onStart(event){
         event.preventDefault();
-        console.log('comecou');
         this.wrapper.addEventListener('mousemove', this.onMove);
+        this.dist.startX = event.clientX;
+        console.log(this.dist.startX);
     }
 
-    onMove(){
-        console.log('moveu');
+    onMove(event){
+        const finalPosition = this.updatePosition(event.clientX);
+        this.moveSlide(finalPosition);
     }
 
     onEnd(){
-        console.log('acabou');
         this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     addSlideEvents(){
@@ -28,6 +46,7 @@ export default class Slide{
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
+        this.updatePosition = this.updatePosition.bind(this);
 
     }
 
